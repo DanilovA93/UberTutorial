b9cc77d0117d8ce1d3cc1a3fd97b845a982d59c1
Anton Danilov <danilovantonne@gmail.com>
2021-05-09 23:55:47 +0300

    complete login UI

--- UberTutorial/Controller/LoginController.swift
+++ UberTutorial/Controller/LoginController.swift
@@ -32,23 +32,51 @@ class LoginController: UIViewController {
         return UITextField.textField(withPlaceholder: "Password", isSecureTextEntry: true)
     }()
     
+    private let logginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Log In", for: .normal)
+        button.setTitleColor(UIColor(white: 1, alpha: 0.5), for: .normal)
+        button.backgroundColor = .mainBlue
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        return button
+    }()
+    
+    private let dontHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        let attributedTitle = NSMutableAttributedString(string: "Don't have an account? ",
+                                                        attributes: [
+                                                            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16),
+                                                            NSAttributedString.Key.foregroundColor: UIColor.lightGray
+                                                        ])
+        attributedTitle.append(NSAttributedString(string: "Sign Up",
+                                                  attributes: [
+                                                    NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16),
+                                                    NSAttributedString.Key.foregroundColor: UIColor.mainBlue
+                                                  ]))
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        
+        return button
+    }()
+    
     //MARK: - Lificycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(displayP3Red: 25/255,
-                                       green: 25/255,
-                                       blue: 25/255,
-                                       alpha: 1)
+        view.backgroundColor = .backgrooundColor
         
         view.addSubview(titleLabel)
         titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor)
         titleLabel.centerX(inView: view)
         
-        let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView])
+        let stack = UIStackView(arrangedSubviews: [emailContainerView,
+                                                   passwordContainerView,
+                                                   logginButton])
         stack.axis = .vertical
         stack.distribution = .fillEqually
-        stack.spacing = 16
+        stack.spacing = 24
+        
         view.addSubview(stack)
         stack.anchor(top: titleLabel.bottomAnchor,
                      left: view.leftAnchor,
@@ -56,6 +84,11 @@ class LoginController: UIViewController {
                      paddingTop: 40,
                      paddingLeft: 16,
                      paddingRight: 16)
+        
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.centerX(inView: view)
+        dontHaveAccountButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                                     height: 32)
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
