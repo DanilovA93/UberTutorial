422f5d5875a9ef8381bde6277500b22e7f6114f9
Anton Danilov <danilovantonne@gmail.com>
2021-06-08 22:14:09 +0300

    set data to rideActionView

--- UberTutorial/Controller/HomeController.swift
+++ UberTutorial/Controller/HomeController.swift
@@ -226,11 +226,16 @@ class HomeController: UIViewController {
         }, completion: completion)
     }
     
-    func animateRideActionView(shouldShow: Bool) {
+    func animateRideActionView(shouldShow: Bool, destination: MKPlacemark? = nil) {
         
         let yOrigign = shouldShow
             ? self.view.frame.height - self.rideActionViewHeight
             : self.view.frame.height
+        
+        if shouldShow {
+            guard let destination = destination else { return }
+            self.rideActionView.destination = destination
+        }
         
         UIView.animate(withDuration: 0.3) {
             self.rideActionView.frame.origin.y = yOrigign
@@ -409,7 +414,7 @@ extension HomeController: UITableViewDelegate, UITableViewDataSource {
             
             self.mapView.showAnnotations(annotations, animated: true)
             
-            self.animateRideActionView(shouldShow: true)
+            self.animateRideActionView(shouldShow: true, destination: selectedPlacemark)
         }
     }
 }
